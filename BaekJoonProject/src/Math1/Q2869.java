605a2f1b0168eb3ee8a74356546c9a3ef7260798
realax122 <realax122@gmail.com>
2021-06-25 17:57:27 +0900

    [수학1[

--- BaekJoonProject/src/Math1/Q2869.java
+++ BaekJoonProject/src/Math1/Q2869.java
@@ -2,15 +2,17 @@ package Math1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 public class Q2869 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
-		int A = Integer.parseInt(s.readLine());
-		int B = Integer.parseInt(s.readLine());
-		int V = Integer.parseInt(s.readLine());
+		StringTokenizer st = new StringTokenizer(s.readLine(), " ");
+		double A = Double.parseDouble(st.nextToken());
+		double B = Double.parseDouble(st.nextToken());
+		double V = Double.parseDouble(st.nextToken());
 		double a = Math.ceil((V-B)/(A-B));
 		System.out.println((int)a);
 	}
@@ -28,3 +30,9 @@ double B = Double.parseDouble(s.readLine());
 double V = Double.parseDouble(s.readLine());
 double a = Math.ceil((V-B)/(A-B));
 System.out.println((int)a); */
+
+/*
+int A = Integer.parseInt(s.readLine());
+int B = Integer.parseInt(s.readLine());
+int V = Integer.parseInt(s.readLine());
+double a = Math.ceil((V-B)/(A-B));*/
